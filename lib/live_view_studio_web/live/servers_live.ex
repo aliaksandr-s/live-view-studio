cef9625de3d518ee9dcd6a4057094594a5cd60e3
Alex S <aliaksandr@lyte.com>
2023-08-19 09:27:46 +0200

    Update

--- lib/live_view_studio_web/live/servers_live.ex
+++ lib/live_view_studio_web/live/servers_live.ex
@@ -2,6 +2,7 @@ defmodule LiveViewStudioWeb.ServersLive do
   use LiveViewStudioWeb, :live_view
 
   alias LiveViewStudio.Servers
+  alias LiveViewStudio.Servers.Server
 
   def mount(_params, _session, socket) do
     servers = Servers.list_servers()
@@ -9,7 +10,7 @@ defmodule LiveViewStudioWeb.ServersLive do
     socket =
       assign(socket,
         servers: servers,
-        coffees: 0
+        coffees: 0,
       )
 
     {:ok, socket}
@@ -27,15 +28,51 @@ defmodule LiveViewStudioWeb.ServersLive do
   end
 
   def handle_params(_, _uri, socket) do
-    {:noreply,
-     assign(
-       socket,
-       selected_server: hd(socket.assigns.servers)
-     )}
+    socket =
+      if socket.assigns.live_action == :new do
+        changeset = Servers.change_server(%Server{})
+
+        assign(socket,
+          selected_server: nil,
+          form: to_form(changeset)
+        )
+      else
+        assign(socket,
+          selected_server: hd(socket.assigns.servers)
+        )
+      end
+
+    {:noreply, socket}
   end
 
   def handle_event("drink", _, socket) do
     {:noreply, update(socket, :coffees, &(&1 + 1))}
+  end
+
+  def handle_event("save", %{"server" => server_params}, socket) do
+    case Servers.create_server(server_params) do
+      {:ok, server} ->
+        socket =
+          update(
+            socket,
+            :servers,
+            fn servers -> [server | servers] end
+          )
+
+        changeset = Servers.change_server(%Server{})
+
+        socket = put_flash(socket, :info, "Server successfully created!")
+        socket = assign(socket, form: to_form(changeset))
+        socket = push_patch(socket, to: ~p"/servers/#{server.id}")
+
+        {:noreply, socket}
+
+      {:error, changeset} ->
+        socket = put_flash(socket, :error, "Error creating server!")
+        socket = assign(socket, form: to_form(changeset))
+
+        {:noreply, socket}
+    end
   end
 
   attr :server, LiveViewStudio.Servers.Server, required: true
@@ -76,6 +113,9 @@ defmodule LiveViewStudioWeb.ServersLive do
     <div id="servers">
       <div class="sidebar">
         <div class="nav">
+          <.link patch={~p"/servers/new"} class="add">
+            + Add New Server
+          </.link>
           <.link
             :for={server <- @servers}
             patch={~p"/servers?#{[id: server]}"}
@@ -95,7 +135,37 @@ defmodule LiveViewStudioWeb.ServersLive do
       </div>
       <div class="main">
         <div class="wrapper">
-          <.server server={@selected_server} />
+          <.form :if={@live_action == :new} for={@form} phx-submit="save">
+            <div class="field">
+              <.input
+                field={@form[:name]}
+                placeholder="Name"
+                autocomplete="off"
+              />
+            </div>
+            <div class="field">
+              <.input
+                field={@form[:framework]}
+                placeholder="Framework"
+                autocomplete="off"
+              />
+            </div>
+            <div class="field">
+              <.input
+                field={@form[:size]}
+                placeholder="Size (MB)"
+                autocomplete="off"
+              />
+            </div>
+            <.button phx-disable-with="Saving...">
+              Save
+            </.button>
+            <.link patch={~p"/servers"} class="cancel">
+              Cancel
+            </.link>
+          </.form>
+
+          <.server :if={@live_action != :new} server={@selected_server} />
 
           <div class="links">
             <.link navigate={~p"/light"}>Adjust light</.link>
