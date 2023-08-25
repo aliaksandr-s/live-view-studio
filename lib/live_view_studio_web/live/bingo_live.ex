e6c1c65ca0902cf3c9b79bb4c5100b63b1764b8d
Alex S <aliaksandr@lyte.com>
2023-08-25 10:58:36 +0200

    Update

--- lib/live_view_studio_web/live/bingo_live.ex
+++ lib/live_view_studio_web/live/bingo_live.ex
@@ -1,8 +1,6 @@
 defmodule LiveViewStudioWeb.BingoLive do
   use LiveViewStudioWeb, :live_view
 
-  on_mount {LiveViewStudioWeb.UserAuth, :ensure_authenticated}
-
   def mount(_params, _session, socket) do
     socket =
       assign(socket,
